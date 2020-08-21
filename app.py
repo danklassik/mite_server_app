e249eb80a4adc831370a5de15a8a44a79fc19c05
danklassik <dan.klassik@gmail.com>
2020-08-21 10:47:20 +0000

    Rebuild for Docker container

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, request
 import subprocess, os
 from glob import glob
+from shutil import copyfile
 
 app = Flask(__name__)
+
+if len(os.listdir('./static/conf') ) == 0:
+    print('Empty CONF directory. Copy demo data!')
+    copyfile('./static/img-demo/*', './static/conf/img/')
+    copyfile('./static/playbooks-demo/*', './static/conf/playbooks/')
+    copyfile('./static/access-demo/*', './static/conf/access/')
+
 
 def find_ext(dr, ext):
     return glob(os.path.join("{}".format(dr),"*.{}".format(ext)))
 
-list_files = find_ext("./ansible","yml")
+
+list_files = find_ext("./static/conf/playbooks","yml")
 for x in range(len(list_files)):
-    print(('{}'+") "+"{}").format(x+1, list_files[x][10:]))
+    print(('{}'+") "+"{}").format(x+1, list_files[x][24:]))
 
 app = Flask(__name__)
 
@@ -32,11 +41,11 @@ def app_data(list):
         else:
             descr.append('')
         f.close()
-        pack.append(list[x][10:-4])
+        pack.append(list[x][24:-4])
     return pack, name, descr
 
 
-app_files = find_files("./ansible","yml")
+app_files = find_files("./static/conf/playbooks","yml")
 app_addr, app_name, app_descr = app_data(app_files)
 
 
@@ -49,7 +58,7 @@ def index():
 @app.route('/<string:work>/<string:prog>',  methods=['GET'])
 def program(work, prog):
     print(work+' of program: '+prog+' from IP: '+request.remote_addr)
-    subprocess.Popen(["ansible-playbook", "./ansible/"+prog+".yml", "-i", request.remote_addr+",", "-i", "conf.txt", "--tags", work],  stdout=subprocess.PIPE)
+    subprocess.Popen(["ansible-playbook", "./static/conf/playbooks/"+prog+".yml", "-i", request.remote_addr+",", "-i", "./static/conf/access/conf.txt", "--tags", work],  stdout=subprocess.PIPE)
     return redirect(url_for('scheduled'))
 
 
@@ -58,4 +67,4 @@ def scheduled():
     return render_template('scheduled.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5001")
+    app.run(host="0.0.0.0", port="5003")
